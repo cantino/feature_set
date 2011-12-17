daba36bbfb160fd53b89b2438cbe8e24761801e0
Andrew Cantino <cantino@gmail.com>
2011-12-17 11:52:15 -0500

    prune idfs

--- lib/feature_set/feature_builder/word_vector.rb
+++ lib/feature_set/feature_builder/word_vector.rb
@@ -23,21 +23,31 @@ module FeatureSet
             end
           end
         end
-
-        num_docs = dataset.length
-        idfs.each do |feature, freqs|
-          freqs.each do |key, value|
-            idfs[feature][key] = Math.log(num_docs / value.to_f)
-          end
-        end
         
-        def generate_features(datum, key, row)
-          return {} unless datum.value.is_a?(String)
-          num_words = datum.tokens.length.to_f
-          idfs[key].inject({}) do |memo, (word, idf)|
-            memo[word] = ((datum.token_counts[word] || 0) / num_words) * idf
-            memo
+        num_docs = dataset.length
+        STDERR.puts "Done building df counts.  The dataset has #{num_docs} documents."
+        pruned = 0
+        idfs.each do |feature, freqs|
+          new_freqs = {}
+          freqs.each do |key, value|
+            log = Math.log(num_docs / value.to_f)
+            if log < 6.0
+              new_freqs[key] = log
+            else
+              pruned += 1
+            end
           end
+          idfs[feature] = new_freqs
+        end
+        STDERR.puts "Done calculating idfs.  Pruned #{pruned} rare values."
+      end
+      
+      def generate_features(datum, key, row)
+        return {} unless datum.value.is_a?(String)
+        num_words = datum.tokens.length.to_f
+        idfs[key].inject({}) do |memo, (word, idf)|
+          memo[word] = ((datum.token_counts[word] || 0) / num_words) * idf
+          memo
         end
       end
     end
